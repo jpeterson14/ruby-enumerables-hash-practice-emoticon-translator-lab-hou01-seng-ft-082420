e2e2bdc5622825630d8da89543be42df33ab34a3
Jake Peterson <jpeterson0514@gmail.com>
2020-07-30 21:09:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def get_japanese_emoticon(file_path,emoticon)
     if value[:english] == emoticon
       return value[:japanese]
 end
-else "Sorry, that emoticon was not found."
+ return "Sorry, that emoticon was not found."
 end
 end 
 
